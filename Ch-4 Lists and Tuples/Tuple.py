9e1bd30c4455f659f5f6fa7080374f1cb7775859
Lakshay <lakshay0sharma@gamil.com>
2022-09-25 18:13:11 +0530

    add tuples and tuples method files

---
+++ Ch-4 Lists and Tuples/Tuple.py
@@ -0,0 +1,26 @@
+# Tuple
+# t = (213,32,4,324,233,2)
+# print(t)
+# print(t[0])
+
+# Empty tuple
+# this is an empty tuple 
+# t = ()
+# print(t)
+
+# tuple with only one element
+# t = (2) this is a worg way to create a tuple with only one element, its confuse python with the integer datatype
+# t = (2,) #this is a corret way to create a tuple with only one element
+# print(t)
+
+# tuples methods 
+# 1. count 
+# t = (1,2,3,4,4)
+# print(t.count(4)) 
+
+# 2. index method
+t = (1,2,3,4,4)
+print(t.index(2)) 
+# the result will be 1 because the value 2 is in the index 1
+
+
